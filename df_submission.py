ec0df9966e66627ed725e6fdb36d55e41e836831
Yasmeen Brain <47681976+YasPHP@users.noreply.github.com>
2021-02-04 21:25:32 -0500

    finalized df submission csv file

--- df_submission.py
+++ df_submission.py
@@ -1,23 +1,27 @@
+# CATHETER COLUMNS
+ETT_COLUMNS = ["ETT - Abnormal", "ETT - Borderline", "ETT - Normal"]
+NGT_COLUMNS = ["NGT - Abnormal", "NGT - Borderline", "NGT - Incompletely Imaged", "NGT - Normal"]
+CVC_COLUMNS = ["CVC - Abnormal", "CVC - Borderline", "CVC - Normal"]
+SWAG_COLUMN = ["Swan Ganz Catheter Present"]
 
-# Create Submission df
-df_submission = pd.DataFrame(np.squeeze(pred)).transpose()
-df_submission.rename(columns=dict(zip([str(i) for i in range(12)], label_cols)))
-df_submission["StudyInstanceUID"] = test_files
+
+# assuming we don't need squeeze and transpose (!!!!!!!!!!!!!!!!!!!!!)
+# Catheter Dataframes
+ETT_df_submission = pd.DataFrame(ETT_pred, columns = ETT_COLUMNS)
+NGT_df_submission = pd.DataFrame(NGT_pred, columns = NGT_COLUMNS)
+CVC_df_submission = pd.DataFrame(CVC_pred, columns = CVC_COLUMNS)
+SWAG_df_submission = pd.DataFrame(SWAG_pred, columns = SWAG_COLUMN)
+
+# StudyInstanceUID DataFrame
+SUID_df_submission = pd.DataFrame(test_files, "StudyInstanceUID")
+
+# concatenated dataframes
+catheter_df = [SUID_df_submission, ETT_df_submission, NGT_df_submission, CVC_df_submission, SWAG_df_submission]
+
+# FINAL SUBMISSION DF
+
+# puts IDs in first dataframe index
+df_submission = pd.concat(catheter_df)
 
 # SUBMISSION FILE
 df_submission.to_csv("submission.csv", index=False)
-
-
-#ETT
-pd.DataFrame(pred, columns = ETT_COLUMNS)
-
-ETT_COLUMNS = df_train["StudyInstanceUID", "ETT - Abnormal", "ETT - Borderline", "ETT - Normal"]
-NGT_COLUMNS = df_train["StudyInstanceUID", "NGT - Abnormal", "NGT - Borderline", "NGT - Incompletely Imaged", "NGT - Normal"]
-CVC_COLUMNS = df_train["StudyInstanceUID", "CVC - Abnormal", "CVC - Borderline", "CVC - Normal"]
-SWAG_COLUMNS = df_train["StudyInstanceUID", "Swan Ganz Catheter Present"]
-
-^ That would be a filtered dataframe of just those columns and their content though. Not just the 4 column labels.
-
-
-# Concatenation
-pd.concat([pred_df], axis = 1)
